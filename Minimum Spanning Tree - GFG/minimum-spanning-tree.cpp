a080dce0fd0ad3a8f33a0dd7a98f0ec3961429f2
Aryan Dhull <99260658+Aryan-Dhull@users.noreply.github.com>
2023-07-13 16:06:43 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,36 +3,96 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
-	public:
-        int spanningTree(int V, vector<vector<int>> adj[]){
+
+class DisJointSet{
+    vector<int> rank,parent,size;
+    
+    public:
+        DisJointSet(int n){
+            rank.resize(n+1,0);
+            size.resize(n+1,1);
+            parent.resize(n+1);
             
-            priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+            for(int i=0;i<=n;i++)parent[i]=i;
+        }
+        
+        int findUpar(int u){
+            if(u==parent[u])return u;
+            else return parent[u]=findUpar(parent[u]);
+        }
+        
+        void unionBySize(int u,int v){
+            int up_u=findUpar(u);
+            int up_v=findUpar(v);
             
-            pq.push({0,0});
-            int sum=0;
-            vector<int> vis(V,0);
+            if(up_u==up_v)return;
             
-            while(!pq.empty()){
-                int node=pq.top().second;
-                int weight=pq.top().first;
-                pq.pop();
-                
-                if(vis[node]==1)continue;
-                vis[node]=1;
-                sum+=weight;
-                
-                for(auto it: adj[node]){
-                    int adjnode=it[0];
-                    int edgeweight=it[1];
-                    if(!vis[adjnode]){
-                        pq.push({edgeweight,adjnode});
-                    }
-                }
+            if(size[up_u]>size[up_v]){
+                parent[up_v]=up_u;
+                size[up_u]+=size[up_v];
             }
             
-            return sum;
+            else{
+                parent[up_u]=up_v;
+                size[up_v]+=size[up_u];
+            }
         }
+        
+        void unionByRank(int u,int v){
+            int up_u=findUpar(u);
+            int up_v=findUpar(v);
+
+            if(up_u==up_v)return;
+            
+            if(rank[up_u]>rank[up_v]){
+                parent[up_v]=up_u;
+            }
+            
+            else if(rank[up_v]>rank[up_u]){
+                parent[up_u]=up_v;
+            }
+            
+            else{
+                parent[up_u]=up_v;
+                rank[up_v]++;
+            }
+        }
+};
+
+class Solution{
+	public:
+    int spanningTree(int V, vector<vector<int>> adj[]){
+        vector<pair<int,pair<int,int>>> edges;
+        
+        for(int i=0;i<V;i++){
+            for(auto it: adj[i]){
+                int adjnode=it[0];
+                int weight=it[1];
+                int node=i;
+                
+                edges.push_back({weight,{node,adjnode}});
+            }   
+        }
+        
+        sort(edges.begin(),edges.end());
+        
+        int mstWt=0;
+        
+        DisJointSet ds(V);
+        
+        for(auto it: edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            
+            if(ds.findUpar(u)!=ds.findUpar(v)){
+                mstWt+=wt;
+                ds.unionBySize(u,v);
+            }
+        }
+        
+        return mstWt;
+    }
 };
 
 //{ Driver Code Starts.
