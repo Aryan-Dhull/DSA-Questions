9923730b4be0767cc83a6f4438ac73c51e887aec
Aryan Dhull <99260658+Aryan-Dhull@users.noreply.github.com>
2023-07-23 19:16:32 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -3,38 +3,36 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution
-{
-	public:
-    	vector<int> topoSort(int V, vector<int> adj[]) {
-    	    int indegree[V]={0};
-    	    
-    	    
-    	    for(int i=0;i<V;i++){
-    	        for(auto it: adj[i]){
-    	            indegree[it]++;
-    	        }
-    	    }
-    	    
-    	    vector<int> ans;
-    	    queue<int> q;
-    	    
-    	    for(int i=0;i<V;i++){
-    	        if(indegree[i]==0)q.push(i);
-    	    }
-    	    
-    	    while(!q.empty()){
-    	        int node=q.front();
-    	        q.pop();
-    	        ans.push_back(node);
-    	        for(auto it: adj[node]){
-    	            indegree[it]--;
-    	            if(indegree[it]==0)q.push(it);
-    	        }
-    	    }
-    	    
-    	    return ans;
-    	}
+class Solution{
+private:
+    void topodfs(int node,vector<int> adj[],stack<int> &st,vector<int> &vis){
+        vis[node]=1;
+        
+        for(auto it: adj[node]){
+            if(!vis[it])topodfs(it,adj,st,vis);
+        }
+        
+        st.push(node);
+    }
+    
+public:
+	vector<int> topoSort(int V, vector<int> adj[]) {
+	    vector<int> vis(V,0);
+	    stack<int> st;
+	    
+	    for(int i=0;i<V;i++){
+	        if(!vis[i])topodfs(i,adj,st,vis);
+	    }
+	    
+	    vector<int> ans;
+	    
+	    while(!st.empty()){
+	        ans.push_back(st.top());
+	        st.pop();
+	    }
+	    
+	    return ans;
+	}
 };
 
 //{ Driver Code Starts.
